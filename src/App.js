6acb5aaf8e1935d0eb1951272f4c89af5452d0cf
adisak <adisak@intelligent-bytes.com>
2019-02-22 17:02:48 +0700

    [Refactor] have btnIn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Container, Header, Segment } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Container textAlign='center' style={{width: '500px'}}>
+        <br /><br />
+        <Segment placeholder>
+          <Header as='h1'>Parking System</Header>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <button className='ui big green button' id="btnIn">Check In</button>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        </Segment>
+      </Container>
     );
   }
 }
